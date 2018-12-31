a7107e79d875e98e960dc751980bf3e1af9e2a27
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-01-01 03:58:44 +0800

    Update msalconfig.js

--- msalconfig.js
+++ msalconfig.js
@@ -1,4 +1,4 @@
 var msalconfig = {
     clientID: "66992713-6f8f-46c1-bf2a-5304dbcc391a",
-    redirectUri: "https://cusuregsoc.ga/main.html"
+    redirectUri: "https://harrykwan.github.io/cuhk-reg-soc/main.html"
 };
